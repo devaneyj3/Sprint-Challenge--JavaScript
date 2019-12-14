ea8590e8c9379c57c45e0c419b2b66057fcdb3d0
Jordan Devaney <jordandevaney28@gmail.com>
2019-12-13 22:21:44 -0500

    complete class refactor minus stretch

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,7 +1,26 @@
 // 1. Copy and paste your prototype in here and refactor into class syntax.
 
+/* ===== Prototype Practice ===== */
+
+// Task: You are to build a cuboid maker that can return values for a cuboid's volume or surface area. Cuboids are similar to cubes but do not have even sides. Follow the steps in order to accomplish this challenge.
+
+
+class CuboidMaker1 {
+    constructor(length, width, height) {
+        this.length = length;
+        this.width = width;
+        this.height = height;
+    }
+    volume() {
+        return this.length * this.width * this.height;
+    }
+    surfaceArea() {
+        return 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
+    }
+}
+    const cuboid1 = new CuboidMaker1(4, 5, 5);
 // Test your volume and surfaceArea methods by uncommenting the logs below:
-// console.log(cuboid.volume()); // 100
-// console.log(cuboid.surfaceArea()); // 130
+console.log(cuboid1.volume()); // 100
+ console.log(cuboid1.surfaceArea()); // 130
 
 // Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
